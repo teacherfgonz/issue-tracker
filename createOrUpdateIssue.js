fba623e698233a951633064efebb9a014c9ea74d
teacherfgonz <teacherfranciscogonz@gmail.comgit config --global --listgit config --global user.email teacherfranciscogonz@gmail.com>
2021-10-11 22:55:25 -0300

    Almost final page

---
+++ createOrUpdateIssue.js
@@ -0,0 +1,36 @@
+const Joi = require("joi");
+const Join = require("joi");
+const mongoose = require("mongoose");
+const config = require("./config");
+const flatJoi = require("./flatJoi");
+const Issue = require("./models/issue");
+
+module.exports = async function createOrUpdateIssue(req, res) {
+    await mongoose.connect(config.mongodb.uri);
+
+    const schema = Joi.object({
+        title: Joi.string().required(),
+        comment: Joi.string().max(200).required(),
+        status: Joi.string().valid("Open", "Closed").required(),
+    });
+
+    const { error } = schema.validate(req.body, { abortEarly: false });
+    const errors = flatJoi(error);
+
+    if (Object.keys(errors).length > 0) {
+        req.flash("errors", JSON.stringify(errors));
+        req.flash("form", JSON.stringify(req.body));
+        res.redirect(req.params.id ? `/edit/${req.params.id}` : "/create")
+        return;
+    }
+
+    if (req.params.id) {
+        await Issue.findOneAndUpdate({ _id: req.params.id }, req.body);
+        req.flash("messages", "The issue was updated succesfully");
+    } else {
+        req.flash("messages", "An issue has been succesfully created");
+        await Issue.create(req.body)
+    }
+
+    res.redirect("/");
+};
--- server.js
+++ server.js
@@ -1,29 +1,45 @@
 const express = require('express');
-const config = require('./config');
 const mongoose = require('mongoose');
-const issue = require('./models/issue');
+const flash = require('connect-flash');
+const session = require('express-session');
 const {urlencoded} = require('body-parser');
+const config = require('./config'); 
+const Issue = require('./models/issue');
+const createOrUpdateIssue = require("./createOrUpdateIssue");
+const showCreateOrEditIssue = require("./showCreateOrEditIssue");
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(urlencoded({extended: true}))
+app.use(urlencoded({extended: true}));
+
+app.use(
+    session({
+        name: config.session.cookieName,
+        secret: config.session.secret,
+        resave: true,
+        saveUninitialized: true
+    })
+);
+
+app.use(flash());
 
 app.get('/', async (req, res) => {
     await mongoose.connect(config.mongodb.uri);
-    const issues = await issue.find({})
-    res.render('issues.list.ejs', { issues })
-})
+    const issues = await Issue.find({})
+    const messages = req.flash("messages")
+    res.render('issues.list.ejs', { issues, messages })
+});
 
-app.get("/create", (req, res) => {
-    res.render("issues.create.ejs")
-})
-
-app.post("/create", async (req, res) => {
-    const {title, comment, status} = req.body
+app.get("/create", showCreateOrEditIssue);
+app.post("/create", createOrUpdateIssue);
+app.get("/edit/:id", showCreateOrEditIssue);
+app.post("edit/:id", createOrUpdateIssue);
+app.post("/delete/:id", async (req, res) => {
     await mongoose.connect(config.mongodb.uri);
-    await issue.create({title, comment, status});
+    await Issue.deleteOne({_id: req.params.id });
+    req.flash("messages", "The issue was deleted succesfully")
     res.redirect("/")
 })
 
