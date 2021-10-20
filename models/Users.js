4761fe8acbb2d55caf1d2db684c02387e0d7aae3
teacherfgonz <teacherfranciscogonz@gmail.comgit config --global --listgit config --global user.email teacherfranciscogonz@gmail.com>
2021-10-20 17:36:23 -0300

    LogIn

---
+++ models/Users.js
@@ -0,0 +1,13 @@
+const { string } = require("joi");
+const mongoose = require("mongoose");
+
+module.exports = mongoose.model(
+    "User",
+    mongoose.Schema({
+        email: {
+            type: String,
+            unique: true,
+        },
+        password: String,
+    })
+);
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+const bcrypt = require("bcrypt");
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport')
 const flash = require('connect-flash');
 const session = require('express-session');
 const {urlencoded} = require('body-parser');
+const LocalStrategy = require("passport-local").Strategy;
 const config = require('./config'); 
 const Issue = require('./models/issue');
+const User = require('./models/Users')
 const createOrUpdateIssue = require("./createOrUpdateIssue");
 const showCreateOrEditIssue = require("./showCreateOrEditIssue");
+const Joi = require("joi");
+const flatJoi = require("./flatJoi");
 
 const app = express();
 
@@ -24,6 +30,87 @@ app.use(
 );
 
 app.use(flash());
+app.use(express.static("public"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+    new LocalStrategy(
+        { usernameField: "email"},
+        async (email, password, done) => {
+            try {
+                await mongoose.connect(config.mongodb.uri);
+                const user = await User.findOne({email})
+
+                if (!user || !bcrypt.compareSync(password, user.password)) {
+                    return done(null, false, { message: "Incorrect Credentials"})
+                }
+
+                return done(null, user); // successful login
+            } catch(err) {
+                done(err);
+            }
+        }
+    )
+)
+
+passport.serializeUser((user, done) => done(null, user._id));
+passport.deserializeUser(async (id, done) => {
+    await mongoose.connect(config.mongodb.uri);
+    const user = await user.findOne({_id: id});
+    done(null, user);
+})
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs", { error: req.flash("error") });
+});
+app.post(
+    "/login",
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/login",
+        failureFlash: true,
+    })
+);
+
+app.get("/register", (req, res) => {
+    res.render("register.ejs")
+});
+
+app.post("/register", async (req, res) => {
+    
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        email: Joi.string().email({minDomainSegments: 2, tlds: {allow: ['com', 'net']}}),
+        password: Joi.string(),
+    });
+
+    const { error } = schema.validate(req.body, { abortEarly: false });
+    const errors = flatJoi(error);
+
+    console.log(errors)
+
+    if (Object.keys(errors).length > 0) {
+        req.flash("errors", JSON.stringify(errors));
+        req.flash("form", JSON.stringify(req.body));
+        res.redirect("/register")
+        return;
+    }
+
+    await mongoose.connect(config.mongodb.uri);
+    const { name, email, password } = req.body;
+    await User.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, 10),
+    });
+    res.redirect("/login");
+})
+
+app.use((req, res) => {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/login");
+});
 
 app.get('/', async (req, res) => {
     await mongoose.connect(config.mongodb.uri);
@@ -42,6 +129,11 @@ app.post("/delete/:id", async (req, res) => {
     req.flash("messages", "The issue was deleted succesfully")
     res.redirect("/")
 })
+
+app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/")
+});
 
 app.listen(config.port, () => {
     console.log(`listening on port ${config.port}`)
